f0bbc1caa286bd7d4b1fd1d617839bc7e17c87a6
angelod1as <angelo@angelodias.com.br>
2019-11-06 08:18:42 -0300

    moving to ssr

--- src/html.js
+++ src/html.js
@@ -1,8 +1,15 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import PropTypes from 'prop-types';
+import parse from 'html-react-parser';
+import Fragment from 'react-dom-fragment';
+
+/* eslint react/destructuring-assignment: 0 */
+/* eslint react/forbid-prop-types: 0 */
+/* eslint react/require-default-props: 0 */
 
 export default function HTML(props) {
-  const beforeHtml = `
+  const serverBeforeHtml = `
   <!--#set var="section" value="#{section}"-->
   <!--#set var="section_slug" value="#{section_slug}"-->
   <!--#set var="special" value="#{special}"-->
@@ -13,40 +20,41 @@ export default function HTML(props) {
   <!--#set var="show_title_header" value="false"-->
   <!--#include virtual="/virtual/3.0/arte/script-app.inc"-->
   `;
-  const insideHead = `<!--#include virtual='/virtual/3.0/arte/head__full-page.inc'-->`;
-  const afterBody = `<!--#include virtual="/virtual/3.0/arte/header__full-page.inc"-->`;
-  const endOfBody = `<!--#include virtual="/virtual/3.0/arte/article-graphic__full-page_after.inc"-->`
+  const serverInsideHead = `<!--#include virtual='/virtual/3.0/arte/head__full-page.inc'-->`;
+  // const serverAfterBody = `<!--#include virtual="/virtual/3.0/arte/header__full-page.inc"-->`;
+  // const serverEndOfBody = `<!--#include virtual="/virtual/3.0/arte/article-graphic__full-page_after.inc"-->`;
   return (
     <>
-    {beforeHtml}
-    <html {...props.htmlAttributes}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        {insideHead}
-        {props.headComponents}
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <noscript key="noscript" id="gatsby-noscript">
-          This app works best with JavaScript enabled.
-        </noscript>
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-        {endOfBody}
-      </body>
-      {afterBody}
-    </html>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: serverBeforeHtml,
+        }}
+      />
+      <html {...props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <div
+            dangerouslySetInnerHTML={{
+              __html: serverInsideHead,
+            }}
+          />
+          {props.headComponents}
+        </head>
+        <body {...props.bodyAttributes}>
+          {props.preBodyComponents}
+          <noscript key="noscript" id="gatsby-noscript">
+            This app works best with JavaScript enabled.
+          </noscript>
+          <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+          {props.postBodyComponents}
+          {/* {parse(serverEndOfBody)} */}
+        </body>
+        {/* {parse(serverAfterBody)} */}
+      </html>
     </>
-  )
+  );
 }
 
 HTML.propTypes = {
@@ -56,4 +64,4 @@ HTML.propTypes = {
   preBodyComponents: PropTypes.array,
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
-}
+};
